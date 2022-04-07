eaafaf21e9dfbb0c5a3a89b3864a3956fe361800
AshikurRahmanMunna <ashikurrahmanmunna3@gmail.com>
2022-04-07 20:49:54 +0600

    Complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 import app from "./firebase.init";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  sendEmailVerification,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
 import { Button, Form } from "react-bootstrap";
 import { useState } from "react";
 
@@ -13,6 +20,8 @@ function App() {
   const [password, setPassword] = useState("");
   const [validated, setValidated] = useState(false);
   const [registered, setRegistered] = useState(false);
+  const [success, setSuccess] = useState("");
+  const [name, setName] = useState("");
   const handleEmailBlur = (e) => {
     setEmail(e.target.value);
   };
@@ -36,68 +45,105 @@ function App() {
     } else if (!/(?=.*?[a-z])/.test(password)) {
       setError("Password should have at least one lowercase letter");
       return;
-    } else if (!/(?=.*?[0-9])/.test(password)) {
-      setError("Password should have at least one number");
-      return;
-    } else if (!/(?=.*?[#?!@$%^&*-])/.test(password)) {
-      setError("Password should have at least one special character");
-      return;
     }
     setError("");
     setValidated(true);
-    if(registered) {
+    if (registered) {
       signInWithEmailAndPassword(auth, email, password)
-      .then((res) => {
-        console.log(res.user);
-        setEmail("");
-        setPassword("");
-        // Notification
-        Notification.requestPermission().then(function (result) {
-          if (result === "granted") {
-            new Notification(
-              `Successfully logged in with ${res.user.email}`
-            );
-          } else if (result === "denied") {
-            return;
+        .then((res) => {
+          console.log(res.user);
+          setEmail("");
+          setPassword("");
+          setSuccess("Login Success");
+          // Notification
+          if (res.user.emailVerified === true) {
+            Notification.requestPermission().then(function (result) {
+              if (result === "granted") {
+                new Notification(
+                  `Successfully logged in with ${res.user.email}`
+                );
+              } else if (result === "denied") {
+                return;
+              }
+            });
           }
+        })
+        .catch((error) => {
+          setError(error.message);
+          console.error(error);
         });
-      })
-      .catch((error) => {
-        setError(error.message);
-        console.error(error);
-      });
-    }
-    else {
+    } else {
       createUserWithEmailAndPassword(auth, email, password)
-      .then((res) => {
-        console.log(res.user);
-        setEmail("");
-        setPassword("");
-        // Notification
-        Notification.requestPermission().then(function (result) {
-          if (result === "granted") {
-            new Notification(
-              `Successfully registered by ${res.user.email}`
-            );
-          } else if (result === "denied") {
-            return;
-          }
+        .then((res) => {
+          console.log(res.user);
+          setEmail("");
+          setPassword("");
+          verifyEmail();
+          setSuccess(`Email Verification sent to ${res.user.email}`);
+          // Notification
+          Notification.requestPermission().then(function (result) {
+            if (result === "granted") {
+              new Notification(`Email verification sent to ${res.user.email}`);
+            } else if (result === "denied") {
+              return;
+            }
+          });
+        })
+        .catch((error) => {
+          setError(error.message);
+          console.error(error);
         });
-      })
-      .catch((error) => {
-        setError(error.message);
-        console.error(error);
-      });
     }
   };
-  const handleRegisteredChange = event => {
+  const handleRegisteredChange = (event) => {
     setRegistered(event.target.checked);
-  }
+  };
+  const verifyEmail = () => {
+    sendEmailVerification(auth.currentUser).then(() => {
+      console.log(`email verification sent`);
+    });
+  };
+  const handlePasswordReset = () => {
+    sendPasswordResetEmail(auth, email).then(() => {
+      console.log("email sent");
+    });
+  };
+  const handleNameBlur = (event) => {
+    setName(event.target.value);
+  };
+  const setUserName = () => {
+    updateProfile(auth.currentUser, {
+      displayName: name,
+    })
+      .then(() => {
+        console.log("Updating name");
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
   return (
     <div>
       <div className="registration w-50 mx-auto">
-        <h2 className="text-primary my-3">Please {registered ? 'Login' : 'Register'}!!</h2>
+        <h2 className="text-primary my-3">
+          Please {registered ? "Login" : "Register"}!!
+        </h2>
         <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+          {!registered && (
+            <Form.Group className="mb-3" controlId="formBasicName">
+              <Form.Label>Your Name</Form.Label>
+              <Form.Control
+                onBlur={handleNameBlur}
+                type="text"
+                placeholder="Enter Your Name"
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                Please provide your name.
+              </Form.Control.Feedback>
+            </Form.Group>
+          )}
+
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control
@@ -127,11 +173,20 @@ function App() {
             </Form.Control.Feedback>
           </Form.Group>
           <Form.Group className="my-3" controlId="formBasicCheckbox">
-            <Form.Check onChange={handleRegisteredChange} type="checkbox" label="Already Registered" />
+            <Form.Check
+              onChange={handleRegisteredChange}
+              type="checkbox"
+              label="Already Registered"
+            />
           </Form.Group>
+          <p className="text-success">{success}</p>
           <p className="text-danger">{error}</p>
+          <Button onClick={handlePasswordReset} variant="link">
+            Forget Password?
+          </Button>
+          <br />
           <Button variant="primary" type="submit">
-            { registered ? 'Login' : 'Register' }
+            {registered ? "Login" : "Register"}
           </Button>
         </Form>
       </div>
